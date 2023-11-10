26b723389bf9aa6ddba62b66bd66a4acd48c6ef3
PiusKariuki <piuskariuki19@gmail.com>
2023-11-10 11:53:39 +0300

    Browser router: using browder router for now until I understand how hash router works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { DataQuery } from '@dhis2/app-runtime'
+import {DataQuery} from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
-import classes from './App.module.css'
-import { Routes, Route, HashRouter } from "react-router-dom";
-import { ConfigProvider } from "antd";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ConfigProvider} from "antd";
 import NavigationLayout from "./shared/layouts/navigationLayout";
 import Home from "./shared/pages/Home";
 
@@ -49,31 +48,31 @@ const MyApp = () => (
             },
         }}
     >
-        <HashRouter>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) return <span>ERROR</span>
-                if (loading) return <span>...</span>
-                return (
-                    <Routes>
-                        <Route
-                            path="/*"
-                            element={
-                                <NavigationLayout
-                                    user={data?.me}
-                                    title={i18n.t("FIND AMS")}
-                                />
-                            }
-                        />
-                        <Route
-                            path="/"
-                            element={<Home  user={data?.me} title={i18n.t("FIND AMS")} />}
-                        />
-                    </Routes>
-                )
-            }}
-        </DataQuery>
-        </HashRouter>
+        <BrowserRouter>
+            <DataQuery query={query}>
+                {({error, loading, data}) => {
+                    if (error) return <span>ERROR</span>
+                    if (loading) return <span>...</span>
+                    return (
+                        <Routes>
+                            <Route
+                                path="/*"
+                                element={
+                                    <NavigationLayout
+                                        user={data?.me}
+                                        title={i18n.t("FIND AMS")}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="/"
+                                element={<Home user={data?.me} title={i18n.t("FIND AMS")}/>}
+                            />
+                        </Routes>
+                    )
+                }}
+            </DataQuery>
+        </BrowserRouter>
     </ConfigProvider>
 )
 
