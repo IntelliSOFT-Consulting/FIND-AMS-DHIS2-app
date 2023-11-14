a6e5e2f46cd5de62a00bb65b35c15f4ffc056c2f
PiusKariuki <piuskariuki19@gmail.com>
2023-11-14 10:18:17 +0300

    AMR-48 Chart details page awaiting endpoint

--- src/modules/Charts/views/SubmittedData.jsx
+++ src/modules/Charts/views/SubmittedData.jsx
@@ -1,25 +1,59 @@
 import {CardItem} from "../../../shared/components/cards/CardItem";
 import {useNavigate} from "react-router-dom";
+import {createUseStyles} from "react-jss";
+
+
+const useStyles = createUseStyles({
+    header: {
+        backgroundColor: "rgb(234, 238, 240)",
+        color: "#1d5288",
+        padding: ".5rem 1rem",
+        fontWeight: "500"
+    },
+    headerContainer: {
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        padding: ".5rem 1rem",
+        marginTop: "2rem",
+        gap: "1rem",
+        "@media (min-width: 768px)": {
+            padding: "1rem 3rem",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "6rem"
+        }
+    },
+    headerComponent: {
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        border: "1px solid",
+
+    },
+    headerItem: {
+        borderRight: ".5px solid",
+        padding: ".5rem .5rem",
+    }
+})
 
 
 export const SubmittedData = () => {
-
+    const styles = useStyles()
     const navigate = useNavigate()
 
-
-    const Header = () => (
-        <div className="card-header">
-            <p className="card-header-text">AMS CHART REVIEW: NEW FORM</p>
-            <button
-                onClick={() => navigate("/charts/submitted-form")}
-                className="primary-btn">START
-            </button>
-        </div>
-    )
-
     return (
-        <CardItem title={Header()}>
+        <CardItem title="AMS CHART REVIEW: FORM XYZZY">
+            <div className={styles.header}>PATIENT DETAILS</div>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerComponent}>
+                    <div className={styles.headerItem}>IP/OP No. :</div>
+                    <div className={styles.headerItem}>123456789</div>
+                </div>
+                <div className={styles.headerComponent}>
+                    <div className={styles.headerItem}>Ward(specialty) :</div>
+                    <div className={styles.headerItem}>Female Ward</div>
+                </div>
 
+
+            </div>
         </CardItem>
     )
 }
