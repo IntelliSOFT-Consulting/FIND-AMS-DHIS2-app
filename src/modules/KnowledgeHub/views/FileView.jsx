469dff3cad58c0584075cbafb84dc71dadbd2bfd
PiusKariuki <piuskariuki19@gmail.com>
2023-11-22 13:22:14 +0300

    Documentation

--- src/modules/KnowledgeHub/views/FileView.jsx
+++ src/modules/KnowledgeHub/views/FileView.jsx
@@ -19,55 +19,82 @@ const query = {
 }
 
 
-
-
 export const FileView = () => {
-    const [document, setDocument] = useState([])
+    /**
+     * State hook to store all form fields and their values for display
+     */
+    const [formElements, setFormElements] = useState([])
 
     const {baseUrl, apiVersion} = useConfig()
 
     const {stages} = useSelector(state => state.knowledgeHub)
 
+    /**
+     * Pdf viewer layout plugin
+     * @type {DefaultLayoutPlugin}
+     */
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
     const {eventId} = useParams()
 
+    /**
+     * Query hook to get event to Id
+     */
     const {data, loading} = useDataQuery(query)
 
 
-
+    /**
+     * Effect hook to invoke a refetch once the eventId is loaded to get the event by UID
+     */
     useEffect(() => {
         query.events.resource = `tracker/events/${eventId}`
     }, [eventId]);
 
+    /**
+     * Effect hook that loads the form elements array with formatted info from the data elements array and the data values array
+     */
     useEffect(() => {
         if (data) {
             data?.events?.dataValues.forEach(dataValue => {
                 const dataElement = getDataElementObjectByID({
                     elementId: dataValue.dataElement, dataElements: stages[0]?.sections[0]?.dataElements
                 })
-                setDocument(prev => [...prev, {...dataElement, value: dataValue.value}])
+                setFormElements(prev => [...prev, {...dataElement, value: dataValue.value}])
             })
         }
     }, [data]);
 
 
-
-    const getDocument = (name) => {
-        const item = document.find(doc => doc.name.toLowerCase().includes(name.toLowerCase()))
+    /**
+     * Gets the object containing the data element from the section array
+     * @param name
+     * @returns {*}
+     */
+    const getFormElement = (name) => {
+        const item = formElements.find(doc => doc.name.toLowerCase().includes(name.toLowerCase()))
         return item
     }
 
+    /**
+     * Engine hook for delete requests
+     * @type {DataEngine}
+     */
     const engine = useDataEngine()
 
+    /**
+     * Download handler
+     * First fetch the blob from DHis2
+     * Then send that blob to the helper function that created an anchor tag dynamically and clicks on it
+     * @returns {Promise<void>}
+     */
     const handleDownload = async () => {
-        const fileName = getDocument("Name")?.value
+        const fileName = getFormElement("Name")?.value
         try {
             const response = await engine.query({
                 events: {
                     resource: "/events/files",
                     params: {
-                        dataElementUid: getDocument("file")?.id,
+                        dataElementUid: getFormElement("file")?.id,
                         eventUid: eventId
                     }
                 }
@@ -83,6 +110,11 @@ export const FileView = () => {
     }
 
 
+    /**
+     * Card header
+     * @returns {JSX.Element}
+     * @constructor
+     */
     const Header = () => {
         const navigate = useNavigate()
 
@@ -119,16 +151,16 @@ export const FileView = () => {
                             <div className={styles.parentWrapper}>
                                 <div className={styles.detailContainer}>
                                     <div className={styles.keyElement}>Document Name:</div>
-                                    <div className={styles.valueElement}>{getDocument("Name")?.value}</div>
+                                    <div className={styles.valueElement}>{getFormElement("Name")?.value}</div>
                                     <div className={styles.keyElement}>Document Permissions:</div>
-                                    <div className={styles.valueElement}>{getDocument("Permission")?.value}</div>
+                                    <div className={styles.valueElement}>{getFormElement("Permission")?.value}</div>
                                     <div className={styles.keyElement}>Category</div>
-                                    <div className={styles.valueElement}>{getDocument("Category")?.value}</div>
+                                    <div className={styles.valueElement}>{getFormElement("Category")?.value}</div>
                                 </div>
 
                                 <div className={styles.detailContainer}>
                                     <div className={styles.keyElement}>Description:</div>
-                                    <div className={styles.valueElement}>{getDocument("Description")?.value}</div>
+                                    <div className={styles.valueElement}>{getFormElement("Description")?.value}</div>
                                 </div>
                             </div>
 
@@ -140,7 +172,7 @@ export const FileView = () => {
                                         Authorization: `Basic ` + btoa("admin" + ":" + "district")
                                     }}
                                     plugins={[defaultLayoutPluginInstance]}
-                                    fileUrl={`${baseUrl}/api/${apiVersion}/events/files?dataElementUid=${getDocument("file")?.id}&eventUid=${eventId}`}
+                                    fileUrl={`${baseUrl}/api/${apiVersion}/events/files?dataElementUid=${getFormElement("file")?.id}&eventUid=${eventId}`}
                                 />
                             </Worker>
                         </>
