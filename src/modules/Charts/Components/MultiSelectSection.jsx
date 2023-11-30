fb7139bbb7d89899676b0952be2e00591bb0b424
PiusKariuki <piuskariuki19@gmail.com>
2023-11-30 09:57:21 +0300

    Required 7-12 questions

--- src/modules/Charts/Components/MultiSelectSection.jsx
+++ src/modules/Charts/Components/MultiSelectSection.jsx
@@ -1,7 +1,7 @@
 import {Checkbox, Form} from "antd";
 import styles from "../styles/Multiselect.module.css"
 
-export const MultiSelectSection = ({section, number, setCheckedValues, initialValues, overrideRequired}) => {
+export const MultiSelectSection = ({section, number, setCheckedValues, initialValues}) => {
     const onChange = (checkedValues) => {
         setCheckedValues(checkedValues)
     };
