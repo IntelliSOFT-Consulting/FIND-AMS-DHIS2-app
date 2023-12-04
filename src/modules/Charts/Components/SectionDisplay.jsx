6782663434221dfec8502c73750dd3baadafa481
PiusKariuki <piuskariuki19@gmail.com>
2023-12-04 09:34:00 +0300

    Displaying the ward display name not the code

--- src/modules/Charts/Components/SectionDisplay.jsx
+++ src/modules/Charts/Components/SectionDisplay.jsx
@@ -1,4 +1,3 @@
-import {formatChartData} from "../../../shared/helpers/formatData";
 import styles from "../styles/ChartDetails.module.css"
 import {generateAlphaNumericList} from "../../../shared/helpers/numbering";
 
@@ -13,23 +12,36 @@ export const SectionDisplay = ({
                                    ordered = false,
                                    alphabet = false
                                }) => {
-    const sectionValues = sectionForms?.dataElements?.map(element => ({
-        name: element?.name || element?.displayName,
-        value: formatChartData({
-            dataElement: element.id,
-            dataValues: data
-        })
-    }))
+
+
+    const sectionValues = sectionForms?.dataElements?.map(element => {
+
+        const item = {
+            name: element?.name,
+        }
+
+        const elementObject = data?.find(dataObject => dataObject?.dataElement === element.id)
+
+        if (element.optionSet) {
+            const option = element.optionSet.options?.find(option => option.code === elementObject?.value)
+            item['value'] = option?.displayName
+        } else {
+            item['value'] = elementObject?.value
+        }
+
+        return item
+
+    })
     return (
         <div className={containerStyles}>
             {sectionValues?.map((item, index) => (
                 <div className={itemsContainerStyles} key={index}>
                     <div className={nameContainerStyles}>
                         <p>
-                            {ordered && !alphabet &&  <span className={styles.number}>
+                            {ordered && !alphabet && <span className={styles.number}>
                                 Question&nbsp;{startingIndex + index}.
                             </span>}
-                            {ordered && alphabet &&  <span className={styles.number}>
+                            {ordered && alphabet && <span className={styles.number}>
                                 Question&nbsp;{startingIndex}{generateAlphaNumericList(index)}.
                             </span>}
 
