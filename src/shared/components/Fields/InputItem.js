a12306f63795f3b3e72dd67f2e0c33094b73c32a
PiusKariuki <piuskariuki19@gmail.com>
2024-01-10 10:55:26 +0300

    Disable future dates

--- src/shared/components/Fields/InputItem.js
+++ src/shared/components/Fields/InputItem.js
@@ -1,56 +1,61 @@
 import React from "react";
-import {Input, Radio, Select, DatePicker, Checkbox, Upload} from "antd";
+import {Checkbox, DatePicker, Input, Radio, Select, Upload} from "antd";
 
-export default function InputItem({ type, name,fileUploadProps, ...props }) {
+export default function InputItem({type, name, fileUploadProps, ...props}) {
 
-  const renderInput = () => {
+    const renderInput = () => {
 
-    switch (type) {
+        const disabledDate = current => {
+            return current && current.valueOf() > Date.now()
+        }
 
-      case "TEXT":
-        return <Input name={name} {...props} />;
+        switch (type) {
 
-      case "BOOLEAN":
-        return (
-          <Radio.Group name={name} {...props}>
-            <Radio value={true}>Yes</Radio>
-            <Radio value={false}>No</Radio>
-          </Radio.Group>
-        );
+            case "TEXT":
+                return <Input name={name} {...props} />;
 
-      case "SELECT":
-        return <Select showSearch name={name} {...props} />;
+            case "BOOLEAN":
+                return (
+                    <Radio.Group name={name} {...props}>
+                        <Radio value={true}>Yes</Radio>
+                        <Radio value={false}>No</Radio>
+                    </Radio.Group>
+                );
 
-      case "RADIO":
-        return <Radio.Group showSearch name={name} {...props} />;
+            case "SELECT":
+                return <Select showSearch name={name} {...props} />;
 
-      case "DATE":
-        return (
-          <DatePicker
-            name={name}
-            {...props}
-            format="YYYY-MM-DD"
-            style={{
-              width: "100%",
-            }}
-          />
-        );
+            case "RADIO":
+                return <Radio.Group showSearch name={name} {...props} />;
 
-      case "MULTI_TEXT":
-        return (
-            <Checkbox.Group  style={{display: "grid", gridTemplateColumns: "1fr 1fr"}} name={name} {...props} />
-        )
+            case "DATE":
+                return (
+                    <DatePicker
+                        disabledDate={disabledDate}
+                        name={name}
+                        {...props}
+                        format="YYYY-MM-DD"
+                        style={{
+                            width: "100%",
+                        }}
+                    />
+                );
 
-      case "LONG_TEXT":
-        return <Input.TextArea rows={4} name={name} {...props} />;
+            case "MULTI_TEXT":
+                return (
+                    <Checkbox.Group style={{display: "grid", gridTemplateColumns: "1fr 1fr"}} name={name} {...props} />
+                )
+
+            case "LONG_TEXT":
+                return <Input.TextArea rows={4} name={name} {...props} />;
 
 
-      case "FILE_RESOURCE":
-        return <Upload.Dragger style={{ width: "100%"}} name={name}  {...fileUploadProps} />;
+            case "FILE_RESOURCE":
+                return <Upload.Dragger style={{width: "100%"}} name={name}  {...fileUploadProps} />;
 
-      default:
-        return <Input name={name} {...props} />;
-    }
-  };
-  return renderInput();
+            default:
+                return <Input name={name} {...props} />;
+        }
+    };
+    return renderInput();
 }
