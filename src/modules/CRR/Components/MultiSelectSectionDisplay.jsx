9c4d2d78172b772e322217180714ef965a3c7396
pius <piuskariuki19@gmail.com>
2024-06-19 10:26:32 +0300

    (chore) Display the other option in the chart details

--- src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
+++ src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
@@ -5,16 +5,22 @@ export const MultiSelectSectionDisplay = ({sectionForms, data, number}) => {
     const selectedOptionsArray = data?.map(item => ({
         name: item.dataValues[0]?.value,
         dataElement: item.dataValues[0]?.dataElement
-    }))?.filter(item =>  item?.dataElement === sectionForms?.dataElements[0]?.id)
+    }))?.filter(item => item?.dataElement === sectionForms?.dataElements.find(de => de.optionSet)?.id)
 
+    const otherOption = sectionForms?.dataElements?.find(de => de?.valueType === "LONG_TEXT")
 
     return (
         <div className={styles.multiSelectWrapper}>
             <p className={styles.multiSelectTitle}>Question&nbsp;{number}.&nbsp;{sectionForms.title}</p>
             {
-                selectedOptionsArray?.map((item, index)=> (
+                selectedOptionsArray?.map((item, index) => (
                     <div key={item.name} className={styles.multiSelectItem}>{item.name}</div>
                 ))
+            }
+            {
+                otherOption && (
+                    <div className={styles.multiSelectItem}>{otherOption.name}</div>
+                )
             }
         </div>
     )
