29a916a5ab1dc58bd21f41e5f8d9014104413aa5
pius <piuskariuki19@gmail.com>
2024-06-19 11:06:45 +0300

    (bug) Display the other option in the chart details

--- src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
+++ src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
@@ -7,8 +7,21 @@ export const MultiSelectSectionDisplay = ({sectionForms, data, number}) => {
         dataElement: item.dataValues[0]?.dataElement
     }))?.filter(item => item?.dataElement === sectionForms?.dataElements.find(de => de.optionSet)?.id)
 
-    const otherOption = sectionForms?.dataElements?.find(de => de?.valueType === "LONG_TEXT")
 
+
+    const otherOption = sectionForms?.dataElements?.find(de => de?.valueType === "LONG_TEXT")
+    // console.log("section forms", sectionForms)
+    // console.log("data", data)
+    console.log("Other DE", otherOption)
+
+    const otherValue = (data?.find(item => item?.dataValues[0]?.dataElement ==otherOption?.id))?.dataValues[0]?.value
+    console.log("value", otherValue)
+
+    // const otherValue = data?.map(item => {
+    //     name
+    // })
+    // console.log("otherOption", otherOption)
+    // console.log("data", data)
     return (
         <div className={styles.multiSelectWrapper}>
             <p className={styles.multiSelectTitle}>Question&nbsp;{number}.&nbsp;{sectionForms.title}</p>
@@ -18,10 +31,14 @@ export const MultiSelectSectionDisplay = ({sectionForms, data, number}) => {
                 ))
             }
             {
-                otherOption && (
-                    <div className={styles.multiSelectItem}>{otherOption.name}</div>
-                )
-            }
-        </div>
+                otherValue && (
+                    <div className={styles.multiSelectItemOtherWrapper} >
+                        <div className={styles.multiSelectItemOtherTitle}>{otherOption.name}</div>
+                        <div
+                            className={styles.textArea}>{otherValue}</div>
+                    </div>
     )
+}
+</div>
+)
 }
