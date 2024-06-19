18838e9faa6291df8469c70fb057930676d29996
pius <piuskariuki19@gmail.com>
2024-06-19 11:07:14 +0300

    (bug) Display the other option in the chart details

--- src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
+++ src/modules/CRR/Components/MultiSelectSectionDisplay.jsx
@@ -8,20 +8,10 @@ export const MultiSelectSectionDisplay = ({sectionForms, data, number}) => {
     }))?.filter(item => item?.dataElement === sectionForms?.dataElements.find(de => de.optionSet)?.id)
 
 
-
     const otherOption = sectionForms?.dataElements?.find(de => de?.valueType === "LONG_TEXT")
-    // console.log("section forms", sectionForms)
-    // console.log("data", data)
-    console.log("Other DE", otherOption)
 
     const otherValue = (data?.find(item => item?.dataValues[0]?.dataElement ==otherOption?.id))?.dataValues[0]?.value
-    console.log("value", otherValue)
 
-    // const otherValue = data?.map(item => {
-    //     name
-    // })
-    // console.log("otherOption", otherOption)
-    // console.log("data", data)
     return (
         <div className={styles.multiSelectWrapper}>
             <p className={styles.multiSelectTitle}>Question&nbsp;{number}.&nbsp;{sectionForms.title}</p>
